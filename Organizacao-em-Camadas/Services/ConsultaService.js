ac98c282febba90b263dffe3b3002a7c2a011f28
Marcella <marcella.oliveira.diasss@gmail.com>
2026-08-25 23:19:12 -0300

    feat: adiciona ConsultaService com regra de negocio inicial

---
+++ Organizacao-em-Camadas/Services/ConsultaService.js
@@ -0,0 +1,34 @@
+const ConsultaRepository = require('../Repositories/ConsultaRepository')
+
+class ConsultaService {
+
+    listarTodos() {
+        return ConsultaRepository.findAll();
+    }
+
+    buscarPorId(id) {
+        return ConsultaRepository.findById(id);
+    }
+
+    criarConsulta(dados) {
+        const consultas = ConsultaRepository.findAll();
+
+        const horarioOcupado = consultas.some( consulta => consulta.medicoId === dados.medicoId && consulta.data === dados.data);
+
+        if(horarioOcupado){
+            throw new Error('O médico já possui uma consulta marcada nesse horário');
+        }
+
+        return ConsultaRepository.create(dados);
+    }
+
+    atualizarConsulta(id, dadosAtualizados) {
+        return ConsultaRepository.update(id, dadosAtualizados);
+    }
+
+    removerConsulta(id) {
+        return ConsultaRepository.delete(id);
+    }
+}
+
+module.exports = new ConsultaService();
