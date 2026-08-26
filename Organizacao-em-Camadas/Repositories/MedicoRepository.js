786d69d70e6d1b8fdb7e8436f9cdbcd1767e5d16
Marcella <marcella.oliveira.diasss@gmail.com>
2026-08-25 22:39:46 -0300

    feat: adiciona pasta de Repositories e adiciona métodos de MedicoRepository.js

---
+++ Organizacao-em-Camadas/Repositories/MedicoRepository.js
@@ -0,0 +1,38 @@
+// Camada responsável por manipular os dados 
+
+const db = require('../db');
+
+class MedicoRepository {
+
+    // listar médicos 
+    findAll() {
+        return db.medicos;
+    }
+
+    // buscar médico por id 
+    findById(id) {
+        return db.medicos.find(medico => medico.id === parseInt(id));
+    }
+
+    // cadastra um novo médico 
+    create(medico) {
+        const novoMedico = { id: Date.now(), ...medico };
+        db.medicos.push(novoMedico);
+        return novoMedico;
+    }
+
+    // atualizar dados do médico 
+    update(id, dadosAtualizados) {
+        const index = db.medicos.findIndex(medico => medico.id === parseInt(id));
+
+        db.medicos[index] = { ...db.medicos[index], dadosAtualizados };
+        return db.medicos[index];
+    }
+
+    // remove médico 
+    delete(id) {
+        const index = db.medicos.findIndex(medico => medico.id === parseInt(id));
+
+        db.medicos.splice(index, 1);
+    }
+}
