b0c7747c363531657a64a1381003ffcb9c9608e0
Marcella <marcella.oliveira.diasss@gmail.com>
2026-08-25 23:50:52 -0300

    feat: adiciona PacienteService.js

---
+++ Organizacao-em-Camadas/Services/PacienteService.js
@@ -0,0 +1,42 @@
+const PacienteRepository = require('../Repositories/PacienteRepository');
+const ConsultaRepository = require('../Repositories/ConsultaRepository');
+
+class PacienteService {
+
+    listarTodos() {
+        return PacienteRepository.findAll();
+    }
+
+    cadastrar(dados) {
+        return PacienteRepository.create(dados);
+    }
+
+    buscarPorId(id) {
+        const paciente = PacienteRepository.findById(id);
+
+        if (!paciente) {
+            throw new Error('Paciente não encontrado');
+        }
+
+        const consultas = ConsultaRepository.findAll().filter(c => c.pacienteId === id);
+
+        return {
+            ...paciente,
+            consultas: consultas
+        };
+    }
+
+    atualizar(id, dados) {
+        return PacienteRepository.update(id, dados);
+    }
+
+    atualizarParcial(id, dadosParciais) {
+        return PacienteRepository.update(id, dadosParciais);
+    }
+
+    remover(id) {
+        return PacienteRepository.delete(id);
+    }
+}
+
+module.exports = new PacienteService();
